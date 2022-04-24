6258995897d5748b5ecc254bc06beea0523e8b95
Suraj Yadav <surajryadav20@gmail.com>
2022-04-24 08:31:14 +0530

    establishing one to many relationships between product and user

--- NODEJS-COMPLETE/app.js
+++ NODEJS-COMPLETE/app.js
@@ -1,6 +1,9 @@
 const path = require("path");
 const sequelize = require("./util/database");
 
+const Product = require("./models/product");
+const User = require("./models/users");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -13,6 +16,7 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const { user } = require("pg/lib/defaults");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -22,10 +26,33 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+//ONE TO MANY RELATIONSHIP
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product); //This line is optional if we write above one
+
 sequelize
+  // .sync({force: true})
   .sync()
   .then((data) => {
-    //console.log(data);
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Suraj", email: "sky@gmail.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    console.log(user);
     app.listen(3000);
   })
   .catch((err) => console.log(err));
--- NODEJS-COMPLETE/util/database.js
+++ NODEJS-COMPLETE/util/database.js
@@ -1,4 +1,4 @@
-const Sequelize = require("sequelize");
+const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize("nodejs", "postgres", "12345", {
   dialect: "postgres",
